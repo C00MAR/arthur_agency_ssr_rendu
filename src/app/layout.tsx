73d036a6f9b9b2f359d8971b7329b3842fb04833
TobogganDev <doretg.t@gmail.com>
2025-01-28 11:41:01 +0100

    Add Lenis SmoothScrolling + GSAP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import SmoothScrolling from "@/components/SmoothScrolling";
 
 export const metadata: Metadata = {
   title: "Arthur Agency",
@@ -12,9 +13,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body>
-        {children}
+        <SmoothScrolling>{children}</SmoothScrolling>
       </body>
     </html>
   );
---
+++ src/components/SmoothScrolling.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { ReactLenis } from "lenis/react";
+
+function SmoothScrolling({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
+      {children}
+    </ReactLenis>
+  );
+}
+
+export default SmoothScrolling;
