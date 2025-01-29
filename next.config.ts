55fda04848cf77fe0d589be50351e4bfaa5e186a
unknown <moureau.marc78@gmail.com>
2025-01-29 14:16:28 +0100

    :sparkles: adding placeholder + some install :sparkles:

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.projetcartylion.fr'], // use for placeholder https://www.projetcartylion.fr/wp-content/uploads/2020/08/Placeholder-600x600.png
+  },
 };
 
 export default nextConfig;
