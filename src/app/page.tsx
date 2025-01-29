21dcfdba18e2e7273cc957777f00abcc35e5e9b2
TobogganDev <doretg.t@gmail.com>
2025-01-29 13:48:44 +0100

    add functions to fetch projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
+import { getAllProjects } from "@/lib/action";
+import { Project } from "./types/Project";
+
+const Home = async () => {
+  const projects: Project[] = await getAllProjects();
+  console.log('[PAGE - PROJECTS]', projects);
   return <></>;
 }
+
+export default Home;
