c2e46ac8823763d64f1a0d42ed4d210b82cfd92b
TobogganDev <doretg.t@gmail.com>
2025-01-30 16:12:20 +0100

    update contact page style

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -61,69 +61,95 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full lg:h-screen flex flex-col-reverse lg:flex-row mt-24 lg:mt-32 px-6 md:px-12">
       <Toaster position="top-right" />
-      <form
-        action={formAction}
-        className="flex flex-col gap-4 w-full max-w-md p-6"
-      >
-        <div>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-            placeholder="Nom"
-            required
-            className="p-2 border rounded-md w-full"
-          />
-          {state.name.errors && (
-            <p className="text-red-500 text-sm">
-              {state.name.errors.join(", ")}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Email"
-            required
-            className="p-2 border rounded-md w-full"
-          />
-          {state.email.errors && (
-            <p className="text-red-500 text-sm">
-              {state.email.errors.join(", ")}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleInputChange}
-            placeholder="Votre message"
-            required
-            className="p-2 border rounded-md w-full min-h-[150px]"
-          />
-          {state.message.errors && (
-            <p className="text-red-500 text-sm">
-              {state.message.errors.join(", ")}
-            </p>
-          )}
-        </div>
-
-        <button
-          className="bg-gray-300 rounded-md p-2 hover:bg-gray-400 transition-colors disabled:opacity-50"
-          disabled={!isValid || isPending}
+      <div className="flex flex-col">
+        <h1 className="text-7xl md:text-9xl mb-12">Contact</h1>
+        <form
+          action={formAction}
+          className="flex flex-col gap-4 md:min-w-[600px]"
         >
-          {isPending ? "Envoi..." : "Envoyer"}
-        </button>
-      </form>
+          <div className="md:flex w-full gap-4 mb-6">
+            <div className="w-full">
+              <label htmlFor="name" className="pb-2 block">
+                Nom
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                placeholder="Nom"
+                required
+                className="mb-4 md:mb-0"
+              />
+              {state.name.errors && (
+                <p className="text-red-500 text-sm">
+                  {state.name.errors.join(", ")}
+                </p>
+              )}
+            </div>
+
+            <div className="w-full">
+              <label htmlFor="email" className="pb-2 block">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                placeholder="Email"
+                required
+              />
+              {state.email.errors && (
+                <p className="text-red-500 text-sm">
+                  {state.email.errors.join(", ")}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="message" className="pb-2 block">
+              Message
+            </label>
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              placeholder="Votre message"
+              required
+              className="py-3 px-5 rounded-lg w-full min-h-[150px]"
+            />
+            {state.message.errors && (
+              <p className="text-red-500 text-sm">
+                {state.message.errors.join(", ")}
+              </p>
+            )}
+          </div>
+
+          <button
+            className="py-3 px-5 rounded-lg bg-black text-white disabled:opacity-50 hover:bg-transparent hover:border-white border border-black transition-all"
+            disabled={!isValid || isPending}
+          >
+            {isPending ? "Envoi..." : "Envoyer"}
+          </button>
+        </form>
+      </div>
+      <div className="lg:mt-48 lg:pl-6 xl:pl-40 mb-12 lg:mb-0">
+        <p className="text-xl">
+          <span className="italic font-normal">Arthur Agency</span> est une
+          agence web créative qui transforme les idées en expériences digitales
+          uniques. Nous concevons des sites web innovants, esthétiques et
+          intuitifs, alliant design percutant et performance optimale. Que vous
+          soyez une startup ou une marque établie, nous donnons vie à votre
+          vision avec passion et expertise. <br />
+          <span className="underline-offset-4 underline font-normal">
+            Créons ensemble quelque chose d’extraordinaire.
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
